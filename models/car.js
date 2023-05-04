83a50a5b05ed20b5ffe2041e90ea2ee6f3a373c0
diegun99 <diegun123456789@gmail.com>
2023-05-03 21:47:39 -0500

    añadir angulos y la posibilidad de que voltee solo en movimiento carro motor completado

--- models/car.js
+++ models/car.js
@@ -11,6 +11,7 @@ class Car{
         this.acceleration = 0.2;
         this.maxSpeed = 3;
         this.friction = 0.05;
+        this.angle = 0;
 
         //controls
         this.controls = new Controls();
@@ -21,52 +22,70 @@ class Car{
      * Mueve el carrito
      */
     update(){
+        this.#move();
+
+    }
+
+    #move(){// hashtag means a private method
         if(this.controls.forward){
-           this.speed += this.acceleration;
-        }
-        if(this.controls.reverse){
-            this.speed -= this.acceleration;
-        }
-
-        if(this.speed>this.maxSpeed){
-            this.speed = this.maxSpeed;
-        }
-        if (this.speed<- this.maxSpeed/2) {
-            this.speed =- this.maxSpeed/2;  
-        }
-        if(this.speed>0){
-            this.speed-= this.friction;
-        }
-        if(this.speed<0){
-            this.speed-= this.friction;
-        }
-
-        console.log(this.speed)
-        this.y -= this.speed;
-
-
-        if (this.controls.left) {
-            this.x -=2;
-        }
-        if(this.controls.right){
-            this.x +=2;
-        }
-
+            this.speed += this.acceleration;
+         }
+         if(this.controls.reverse){
+             this.speed -= this.acceleration;
+         }
+ 
+         if(this.speed>this.maxSpeed){
+             this.speed = this.maxSpeed;
+         }
+         if (this.speed<- this.maxSpeed/2) {
+             this.speed =- this.maxSpeed/2;  
+         }
+         if(this.speed>0){
+             this.speed-= this.friction;
+         } 
+         if(this.speed<0){
+             this.speed+= this.friction;
+         }
+ 
+         if(Math.abs(this.speed)<this.friction){
+             this.speed = 0;
+         }
+ 
+ 
+         if(this.speed!=0){
+             const flip = this.speed>0?1:-1;// if the value of this flip is one or minus one depending on the speed
+ 
+             if (this.controls.left) {
+                 this.angle+= 0.03*flip;
+             }
+             if(this.controls.right){
+                 this.angle-= 0.03*flip;
+             }
+ 
+         }
+ 
+         this.x -= Math.sin(this.angle)*this.speed;
+         this.y -= Math.cos(this.angle)*this.speed;
+ 
     }
 
 
     draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y); // we translate to the poin where we want the rotation to be centered at
+        ctx.rotate(-this.angle);//tell the context to rotate by minus this angle
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         )
 
             // metodo pars dibujar se usa el fill para mostrar lo dibujado en el canvas
-        ctx.fill()
+        ctx.fill();
 
+        ctx.restore();
     }
 
 }
