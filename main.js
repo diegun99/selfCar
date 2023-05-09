59c774f1b06c087b2059e8c8078eb72c2a424851
diegun99 <diegun123456789@gmail.com>
2023-05-08 21:45:06 -0500

    clase camino creado

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ canvas.width = 200;
 
 //contexto y car
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width/2, canvas.width * 0.9)
 const car = new Car(100,100,30,50);
 car.draw(ctx);
 
@@ -13,7 +14,9 @@ animate();
 //método recursivo para refrescar la pantalla una y otra vez
 function animate (){
     car.update();
+
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
