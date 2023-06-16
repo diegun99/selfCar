afc14ce5b599f8960e6dfc62eb3261c0423c4dbc
diegun99 <diegun_99@hotmail.com>
2023-06-16 13:31:15 -0500

    Añadido el road, se uso la formula de interpolacion lienal que resula interesante indagar mas sobre su uso practico, tambien se le añadio bordes dinamicos y se genero el movimiento de camara,.. ahora sigue el step 3

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ canvas.width = 200;
 //contexto y car
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2, canvas.width * 0.9)
-const car = new Car(100,100,30,50);
+const car = new Car(road.getLaneCenter(1),100,30,50);
 car.draw(ctx);
 
 animate();
@@ -16,7 +16,13 @@ function animate (){
     car.update();
 
     canvas.height = window.innerHeight;
+
+    ctx.save();// movimiento camara siguiendo al car 
+    ctx.translate(0,-car.y+canvas.height*0.7);
+
     road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
--- models/road.js
+++ models/road.js
@@ -11,26 +11,53 @@ class Road{
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+        // detectar bordes del road , se deber tener en cuenta todas las posibilidades : por ejemplo , establecer si habran bordes en el medio de los carriles o si tendra curvas la carretera, etc
+        const topLeft = {x:this.left, y:this.top};
+        const topRight = {x:this.right, y:this.top};
+        const bottomLeft = {x:this.left, y:this.bottom};
+        const bottomRight = {x:this.right, y:this.bottom};
+        this.borders = [
+            [topLeft,bottomLeft],// cada array es un segmentode los bordes
+            [topRight,bottomRight]
+        ];
+
+    }
+
+    // UN MÉTODO QUE NOS DIGA CUAL ES EL CENTRO DE CADA CARRIL
+    getLaneCenter(laneIndex){
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneWidth/2 + 
+        Math.min(laneIndex, this.laneCount-1)*laneWidth;// elige el carril minimo posible
     }
 
     draw (ctx){
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        for(let i = 0 ; i<= this.laneCount; i++){
+        for(let i = 1 ; i<= this.laneCount-1; i++){
             const x = lerp(
                 this.left,
                 this.right,
-                i/this.laneCount
+                i/this.laneCount // CONTADOR DE CARRILES 
                 )
             
-   
+
+        ctx.setLineDash([20,20]);
         ctx.beginPath();
         ctx.moveTo(x,this.top);
         ctx.lineTo(x,this.bottom);
         ctx.stroke();
 
     }
+
+    ctx.setLineDash([]);
+    this.borders.forEach(border =>{
+        ctx.beginPath();
+        ctx.moveTo(border[0].x,border[0].y);
+        ctx.lineTo(border[1].x,border[1].y);
+        ctx.stroke();
+    })
 
     }
     
