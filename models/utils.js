575b56f736c946d0e1cc3df9092124f1dc0cd8d1
diegun99 <diegun_99@hotmail.com>
2023-09-18 13:56:24 -0500

    polygon interseccion y formas añadido

--- models/utils.js
+++ models/utils.js
@@ -21,3 +21,21 @@ function lerp(A,B,t){
 
     return null;
 }
+
+function polysIntersect(poly1, poly2){
+    for(let i=0; i<poly1.length ; i++){
+        for(let j = 1; j< poly2.length; j++){
+            const touch = getIntersection(
+                poly1[i],
+                poly1[(i+1)%poly1.length],
+                poly2[j],
+                poly2[(j+1)%poly2.length]
+            );
+            if(touch){
+                return true;
+            }
+        }
+    }
+    return false;
+
+}
